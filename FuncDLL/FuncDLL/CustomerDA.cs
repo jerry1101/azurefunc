a08c003dfa6a996c6ed5ab7072dc1d65986930f7
jerry <jerry110160@gmail.com>
2018-12-15 13:16:59 -0500

    Add DA and VO

---
+++ FuncDLL/FuncDLL/CustomerDA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FuncDLL
+{
+    public class CustomerDA : ICustomerDA
+    {
+        private List<CustomerVO> customerList;
+        public CustomerDA()
+        {
+            customerList = new List<CustomerVO>();
+            customerList.Add(new CustomerVO { ID = 1, FirstName = "A", LastName = "AL" });
+            customerList.Add(new CustomerVO { ID = 2, FirstName = "B", LastName = "BL" });
+            customerList.Add(new CustomerVO { ID = 3, FirstName = "C", LastName = "CL" });
+        }
+
+        List<CustomerVO> ICustomerDA.GetAllCustomer() => customerList;
+
+
+
+        CustomerVO ICustomerDA.GetCustomerByID(int id) => customerList.Find(match: c => c.ID == id);
+
+
+    }
+}
---
+++ FuncDLL/FuncDLL/ICustomerDA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FuncDLL
+{
+    interface ICustomerDA
+    {
+        List<CustomerVO> GetAllCustomer();
+        CustomerVO GetCustomerByID(int id);
+
+    }
+}
